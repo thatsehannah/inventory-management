cae4f9959f36bff8d9ba5b4f5db951f32cd3f25d
E. Hannah <echannah631@gmail.com>
2025-08-19 11:26:10 -0400

    creating navbar and dashboard wrapper for consistent layout across pages

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import DashboardWrapper from "./DashboardWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <DashboardWrapper>{children}</DashboardWrapper>
       </body>
     </html>
   );
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
-  return (
-    <div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
-      Hello
-    </div>
-  );
+  return <Dashboard />;
 }
